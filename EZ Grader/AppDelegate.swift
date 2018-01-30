93697916eba00cecad432bf0c82c954d74ccb381
Rolandas Burbulis <rolandasburbulis@gmail.com>
2018-01-30 00:30:35 -0500

    Log message on app launch Add button to UI that logs to console when tapped

--- EZ Grader/AppDelegate.swift
+++ EZ Grader/AppDelegate.swift
@@ -11,12 +11,11 @@ import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        print("Application did finish launching")
         return true
     }
 
