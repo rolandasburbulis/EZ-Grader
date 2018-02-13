b8b89847f664eefab543be224c98b8286d5d3a35
Rolandas Burbulis <rolandasburbulis@gmail.com>
2018-02-13 02:34:44 -0500

    using PDFKit to view pdf, view first/last page, page count, add dummy annotation

--- EZ Grader/ViewController.swift
+++ EZ Grader/ViewController.swift
@@ -6,14 +6,24 @@
 //  Copyright © 2018 RIT. All rights reserved.
 //
 
-import UIKit
+//import UIKit
+import PDFKit
 
 class ViewController: UIViewController {
+    // store our PDFView in a property so we can manipulate it later
+    var pdfView: PDFView!
     let pdfFileName = "swift_tutorial"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let firstPageBtn = UIBarButtonItem(title: "First", style: .plain, target: self, action: #selector(firstPage))
+        let lastPageBtn = UIBarButtonItem(title: "Last", style: .plain, target: self, action: #selector(lastPage))
+        let numberOfPagesBtn = UIBarButtonItem(title: "Number of pages", style: .plain, target: self, action: #selector(numberOfPages))
+        let annotationsBtn = UIBarButtonItem(title: "Annotations", style: .plain, target: self, action: #selector(annotations))
+        
+        navigationItem.rightBarButtonItems = [firstPageBtn, lastPageBtn, numberOfPagesBtn, annotationsBtn]
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,17 +32,66 @@ class ViewController: UIViewController {
     }
     
     @IBAction func openPDFAction(_ sender: Any) {
-        if let url = Bundle.main.url(forResource: pdfFileName, withExtension: "pdf") {
-            let webView = UIWebView(frame: self.view.frame)
-            let urlRequest = URLRequest(url: url)
-            webView.loadRequest(urlRequest as URLRequest)
-            self.view.addSubview(webView)
-            
-            let pdfViewController = UIViewController()
-            pdfViewController.view.addSubview(webView)
-            pdfViewController.title = pdfFileName
-            self.navigationController?.pushViewController(pdfViewController, animated: true)
+        // create and add the PDF view
+        pdfView = PDFView()
+        pdfView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(pdfView)
+        
+        // make it take up the full screen
+        pdfView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        pdfView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        pdfView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        pdfView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        // load our example PDF and make it display immediately
+        let url = Bundle.main.url(forResource: pdfFileName, withExtension: "pdf")!
+        
+        pdfView.document = PDFDocument(url: url)
+    }
+    
+    @objc func firstPage() {
+        pdfView.goToFirstPage(nil)
+    }
+    
+    @objc func lastPage() {
+        pdfView.goToLastPage(nil)
+    }
+    
+    @objc func numberOfPages() {
+        print(pdfView.document?.pageCount ?? 0)
+        
+        let fileManager = FileManager.default
+        
+        // Get contents in directory: '.' (current one)
+        
+        do {
+            let files = try fileManager.contentsOfDirectory(atPath: Bundle.main.bundlePath)
+                
+                //+ "/EZ Grader")
+            print(files)
+        }
+        catch let error as NSError {
+            print("Ooops! Something went wrong: \(error)")
         }
     }
-}
+    
+    @objc func annotations() {
+        print(pdfView.document?.page(at: 0)?.annotations.count)
 
+        let rect = CGRect(x: 100.0, y: 100.0, width: 100.0, height: 100.0)
+        
+        let annotation = PDFAnnotation(bounds: rect, forType: .ink, withProperties: nil)
+        annotation.backgroundColor = .blue
+        
+        let pathRect = CGRect(x: 0.0, y: 0.0, width: 100.0, height: 100.0)
+        let path = UIBezierPath(ovalIn: pathRect)
+        annotation.add(path)
+        
+        // Add annotation to the first page
+        pdfView.document?.page(at: 0)?.addAnnotation(annotation)
+        
+
+        
+        print(pdfView.document?.page(at: 0)?.annotations.count)
+    }
+}
